52b57887246079b91da7dc77dd30eae9974694f6
Omar Ahmed Mohammed Mclad <162033259+omarmclad@users.noreply.github.com>
2025-08-30 10:38:58 +0300

    Create 0002.py

---
+++ 0002.py
@@ -0,0 +1,23 @@
+pi=3.14 #float
+q=False #bool
+name="Omar" #string
+salary=1000 #int
+#################
+v=[1,2,3,4,5] #list
+ids={1:"a",2:"b",3:"c"} #dict
+tup=(1,2,3,4,5) #tuple
+#################
+for i in v:
+    print(i)
+        #
+if(v[2]==4):
+    print("true")
+elif(v[2]==5):
+    print("true")
+else:
+    print("false")
+        #
+i=0
+while i<10:
+    print(i)
+    i+=1
